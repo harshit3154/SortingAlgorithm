ae4269339fdeaaf45d4d4c2f8b2950cf69b97c75
harshit kumar <harshitkr9696@gmail.com>
2024-06-12 23:55:08 +0530

    adding merge sort file

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -22,16 +22,6 @@ void merge(int *p,int l,int r,int mid){
         p[f++]=R[j++];
 }
 
-void mergeSort(int *p,int l,int r){
-    if(l<r){
-
-        int mid=l+(r-l)/2;
-        mergeSort(p,l,mid);
-        mergeSort(p,mid+1,r);
-        merge(p,l,r,mid);
-    }
-}
-
 void display(int *p,int n){
     cout<<"\n\n\t\tThe element of the array are \n\n\t\t: ";
     for(int i=0;i<n;i++){
@@ -39,8 +29,49 @@ void display(int *p,int n){
     }
 }
 
+void merg1(int *p,int l,int r,int mid){
+	display(p,3);
+	int m=l;
+	int n1=mid-l+1;
+	int n2=r-mid;
+	
+	int i=0,j=0;
+	
+	while(i<n1 && j<n2){
+		p[m++]=min(p[l],p[mid+1]);
+		if(p[l]<p[mid+1]){
+			l++;
+			i++;
+		}else{
+			mid++;
+			j++;
+		}
+	}
+	
+	while(i<n1){
+		p[m++]=p[l++];
+		i++;
+	}
+	while(j<n2){
+		p[m++]=p[mid++];
+		j++;
+	}
+	
+}
+
+void mergeSort(int *p,int l,int r){
+    if(l<r){
+
+        int mid=l+(r-l)/2;
+        mergeSort(p,l,mid);
+        mergeSort(p,mid+1,r);
+        merg1(p,l,r,mid);
+    }
+    display(p,3);
+}
+
 int main(){
-    int ar[]={5,4,6,8,2,45,24,56,78};
+    int ar[]={5,4,6};
     int n=sizeof(ar)/sizeof(ar[0]);
     display(ar,n);
     mergeSort(ar,0,n-1);
